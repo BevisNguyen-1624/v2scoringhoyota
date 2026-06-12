4e4b5bb8c64ddb4c81505173f799e820919940c6
BevisNguyen1624 <nguyenhuybinh021@gmail.com>
2026-06-12 17:44:58 +0700

    13

--- src/imports/pasted_text/api-helpers.tsx
+++ src/imports/pasted_text/api-helpers.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import bgImage from "../KV_HÒ_YO_TA-01.jpeg";
 
-const API_URL = "https://script.google.com/macros/s/AKfycbwrjZHB_NHaByKCncdJUqNu9d9KM6FZjPmiY9pKVP6vW5OLZspPNtwC9Q7k0xvv4irb/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxf0ZwaK3rK-nK0m5aGsvJFzK63dIbzdyGS-0A_UQc55MShs-sVyHmbFIec2LuMWRQx/exec";
 const USE_MOCK = false;
 const ADMIN_PIN = "1234";
 const LS_KEY = "hoYoTa_reviewers";
